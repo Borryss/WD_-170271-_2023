e710eef0910181cfba52297084ba89729bc7a328
Borryss <115550381+Borryss@users.noreply.github.com>
2023-02-24 09:40:42 +0100

    Add files via upload

---
+++ CW1.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = "*" * 19
+print((a + "\n") * 4)
+b = "*"
+c = "                 "
+
+print(a + "\n" + (b + c + b) + "\n" + (b + c + b) + "\n" + a + "\n")
+print(b + "\n" + b + b + "\n" + b+b+b + "\n" + b + b + b + b + "\n")
+
+d = (512-282)/(47*48 + 5)
+print(d)
+
+e = 7
+f = '---'
+print(str(e) + f + str(e * 2) + f + str(e * 3) + f + str(e * 4) + f + str(e * 5) + "\n")
+
+g = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz " \
+    "pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później " \
+    "zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach " \
+    "60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym " \
+    "różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, " \
+    "jak Aldus PageMaker "
+print(g + "\n")
+
+h = eval(input("Wpisz liczbe:"))
+y = eval(input("Wpisz liczbe:"))
+print(h * y)
+
+x = eval(input("Wpisz liczbe kilogramow:")) * 2.2
+print("To samo w funtach:" + str(x))
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
