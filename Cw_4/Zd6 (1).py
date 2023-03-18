f3abaf4929d737be86974a969e9ad7a6496c6430
Borryss <115550381+Borryss@users.noreply.github.com>
2023-03-18 20:09:44 +0100

    Add files via upload

---
+++ Cw_4/Zd6 (1).py
@@ -0,0 +1,22 @@
+
+
+import numpy as np
+
+
+a = np.array(list('Mleko')).reshape(1,5)
+b = np.array(list('a    ')).reshape(1,5)
+c = np.array(list('k    ')).reshape(1,5)
+d = np.array(list('aa   ')).reshape(1,5)
+e = np.array(list('r s  ')).reshape(1,5)
+f = np.array(list('o  z ')).reshape(1,5)
+g = np.array(list('n   a')).reshape(1,5)
+print("",a,"\n",b,"\n",c,"\n",d,"\n",e,"\n",f,"\n",g) 
+
+
+
+
+
+
+
+
+
