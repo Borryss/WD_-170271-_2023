1453f125bf04736e4ce6aca23368e253daa87558
Borryss <115550381+Borryss@users.noreply.github.com>
2023-04-13 22:47:03 +0200

    Add files via upload

---
+++ CW_5/ZD11.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+
+tab = np.array([1, 2, 5, 8, 4, 2, 6, 7, 4, 8, 4, 6])
+print(tab)
+tab2 = np.array(tab).reshape(3,4)
+print(tab2)
+tab3 = np.array(tab).reshape(4,3)
+print(tab3)
+tab4 = np.array(tab).reshape(2,6)
+print(tab4 ,"\n")
+tab2 = sum(tab2)
+tab2 = sum(tab2)
+print(tab2)
+tab3 = sum(tab3)
+tab3 = sum(tab3)
+print(tab3)
+tab4 = sum(tab4)
+tab4 = sum(tab4)
+print(tab4)
+
+
+
+
+
+
+
+
+
+
+
+
